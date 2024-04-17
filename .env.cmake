fd207950bfbd96c95ba47ad012d5525c4b2275ae
Ernest326 <ernest326648@gmail.com>
2024-04-17 17:55:01 +0100

    Compiling using mingw cause its faster ngl

--- .env.cmake
+++ .env.cmake
@@ -1,3 +1,5 @@
-set(GLFW_PATH ./Libraries/glfw-3.4)
-set(GLM_PATH ./Libraries/glm)
+set(GLFW_PATH C:/Users/Ernest/Documents/SparkEngine/Libraries/glfw-3.4)
+set(GLM_PATH C:/Users/Ernest/Documents/SparkEngine/Libraries/glm)
 set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.280.0)
+
+set(MINGW_PATH "C:/msys64/ucrt64")
---
+++ build/CMakeFiles/SparkEngine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SparkEngine.dir/src/App/app.cpp.obj"
+  "CMakeFiles/SparkEngine.dir/src/App/app.cpp.obj.d"
+  "CMakeFiles/SparkEngine.dir/src/App/window.cpp.obj"
+  "CMakeFiles/SparkEngine.dir/src/App/window.cpp.obj.d"
+  "CMakeFiles/SparkEngine.dir/src/main.cpp.obj"
+  "CMakeFiles/SparkEngine.dir/src/main.cpp.obj.d"
+  "SparkEngine.exe"
+  "SparkEngine.exe.manifest"
+  "SparkEngine.pdb"
+  "libSparkEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SparkEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
